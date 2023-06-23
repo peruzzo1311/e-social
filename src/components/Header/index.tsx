07840cb43b9bbde20ab4aaa0b8db0abae0482de9
Gustavo Peruzzo <gustavoperuzzo1311@hotmail.com>
2023-06-23 13:58:31 -0300

    modified:   src/components/Header/index.tsx 	modified:   src/screens/Messenger/index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,72 +3,160 @@ import { useNavigation } from '@react-navigation/native'
 import { Box, Icon, Image, Button, Text } from 'native-base'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 const logoPrisma = require('../../assets/images/logo-prisma.png')
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons'
 import { useState } from 'react'
 
-export default function Header({opcao} : {opcao:number}) {
-  const navigation : any = useNavigation();
+export default function Header({ opcao }: { opcao: number }) {
+  const navigation: any = useNavigation()
   const [theme, setTheme] = useState(false)
-  if(opcao == 0) {
-    return (
-      <Box
-        safeAreaTop
-        bgColor={'#0171BB'}
-        h={16}
-        flexDir={'row'}
-        justifyContent={'space-between'}
-        alignItems={'center'}
-        px={4}
-      >
-        <Image
-          source={logoPrisma}
-          alt='Logo Prisma'
-          resizeMode='cover'
+
+  switch (opcao) {
+    case 0:
+      return (
+        <Box
+          safeAreaTop
+          bgColor={'#0171BB'}
           h={16}
-          w={175}
-        />
-    <TouchableOpacity onPress={() => {navigation.navigate('Perfil')}}>
-        <Icon as={Ionicons} name='person-circle' size={12} color={'white'} />
-    </TouchableOpacity>
-      </Box>
-    )
-  } else if (opcao == 1){
-    return (
-      <Box
-        safeAreaTop
-        bgColor={'#0171BB'}
-        h={16}
-        flexDir={'row'}
-        justifyContent={'space-between'}
-        alignItems={'center'}
-        px={4}
-      >
-    <TouchableOpacity onPress={() => {navigation.goBack()}}>
-        <AntDesign name='arrowleft' size={36} color={'white'} />
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => setTheme(!theme)}>
-    <Icon as={Ionicons} name={theme ? 'sunny-outline' : 'moon-outline'}  size={10} color={'white'} />
-    </TouchableOpacity>
-      </Box>
-    )
-  }
-  else {
-    return (
-      <Box
-        safeAreaTop
-        bgColor={'#0171BB'}
-        h={16}
-        flexDir={'row'}
-        justifyContent={'space-between'}
-        alignItems={'center'}
-        px={4}
-      >
-    <TouchableOpacity onPress={() => {navigation.goBack()}}>
-        <AntDesign name='close' size={36} color={'white'} />
-    </TouchableOpacity>
-    
-    <Button bgColor={'white'} w={'30%'}><Text fontWeight={'bold'}color={'#0171BB'}>Publicar</Text></Button>
-      </Box>
-    )
+          flexDir={'row'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          px={4}
+        >
+          <Image
+            source={logoPrisma}
+            alt='Logo Prisma'
+            resizeMode='cover'
+            h={16}
+            w={175}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Perfil')
+            }}
+          >
+            <Icon
+              as={Ionicons}
+              name='person-circle'
+              size={12}
+              color={'white'}
+            />
+          </TouchableOpacity>
+        </Box>
+      )
+    case 1:
+      return (
+        <Box
+          safeAreaTop
+          bgColor={'#0171BB'}
+          h={16}
+          flexDir={'row'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          px={4}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack()
+            }}
+          >
+            <AntDesign name='arrowleft' size={28} color={'white'} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setTheme(!theme)}>
+            <Icon
+              as={Ionicons}
+              name={theme ? 'sunny-outline' : 'moon-outline'}
+              size={8}
+              color={'white'}
+            />
+          </TouchableOpacity>
+        </Box>
+      )
+    case 2:
+      return (
+        <Box
+          safeAreaTop
+          bgColor={'#0171BB'}
+          h={16}
+          flexDir={'row'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          px={4}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack()
+            }}
+          >
+            <AntDesign name='close' size={36} color={'white'} />
+          </TouchableOpacity>
+
+          <Button bgColor={'white'} w={'30%'}>
+            <Text fontWeight={'bold'} color={'#0171BB'}>
+              Publicar
+            </Text>
+          </Button>
+        </Box>
+      )
+    case 3:
+      return (
+        <Box
+          safeAreaTop
+          bgColor={'#0171BB'}
+          h={16}
+          flexDir={'row'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          px={4}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack()
+            }}
+          >
+            <AntDesign name='arrowleft' size={28} color={'white'} />
+          </TouchableOpacity>
+
+          <Image
+            source={logoPrisma}
+            alt='Logo Prisma'
+            resizeMode='cover'
+            h={16}
+            w={175}
+          />
+        </Box>
+      )
+      break
+    default:
+      return (
+        <Box
+          safeAreaTop
+          bgColor={'#0171BB'}
+          h={16}
+          flexDir={'row'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          px={4}
+        >
+          <Image
+            source={logoPrisma}
+            alt='Logo Prisma'
+            resizeMode='cover'
+            h={16}
+            w={175}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Perfil')
+            }}
+          >
+            <Icon
+              as={Ionicons}
+              name='person-circle'
+              size={12}
+              color={'white'}
+            />
+          </TouchableOpacity>
+        </Box>
+      )
   }
 }
--- src/screens/Messenger/index.tsx
+++ src/screens/Messenger/index.tsx
@@ -1,8 +1,11 @@
 import { View, Text } from 'react-native'
+import Header from '../../components/Header'
 
 export default function Messenger() {
   return (
     <View>
+      <Header opcao={3} />
+
       <Text>Messenger</Text>
     </View>
   )
