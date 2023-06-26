afe0e99a1a963b6a07407e940fb3958b6854e50f
Matheus Soares <matheus.soares@prismainformatica.com.br>
2023-06-26 16:44:54 -0300

    Criado BackEnd para messenger do app e-social

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -6,15 +6,24 @@ import { TouchableOpacity } from 'react-native';
 import Header from '../../components/Header';
 import { io } from 'socket.io-client';
 const Chat = (props: any) => {
-  const socket = io("http://192.168.1.140:8082")
+  const socket = io('http://192.168.1.140:8082');
+
+  const [mensageTxt, setMensageTxt] = React.useState<string>('');
+
+  const sendMensage = () => {
+    socket.emit('chat mensage', mensageTxt);
+    setMensageTxt('');
+  };
+
   React.useEffect(() => {
-    socket.on("connect", () => {
+    socket.on('connect', () => {
       console.log(socket.connected); // true
     });
     socket.on('hello', (mensage: string) => {
-      console.log(mensage); 
+      console.log(mensage);
     });
   }, []);
+
   return (
     <View flex={1} w={'100%'}>
       <Header opcao={4} />
@@ -35,12 +44,14 @@ const Chat = (props: any) => {
         </View>
         <View w={'90%'} mb={4}>
           <Input
+            value={mensageTxt}
+            onChangeText={(text) => setMensageTxt(text)}
             borderRadius={25}
             placeholder={'Procurar Conversas ...'}
             InputRightElement={
-              <TouchableOpacity style={{ paddingRight: 4 }}>
+              <TouchableOpacity style={{ paddingRight: 4 }} onPress={() => sendMensage()}>
                 <Icon
-                  as={<MaterialIcons name={'mic'} />}
+                  as={<MaterialIcons name={mensageTxt == '' ? 'mic' : 'send'} />}
                   size={5}
                   color="primary.500"
                 />
